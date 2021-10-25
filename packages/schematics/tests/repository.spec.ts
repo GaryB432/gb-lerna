49424a098c146ec2a7026e56c1a1e6e1adc26a96
Gary Bortosky <garybort@gmail.com>
2021-10-25 11:17:03 -0500

    update generated dependencies

--- packages/schematics/tests/repository.spec.ts
+++ packages/schematics/tests/repository.spec.ts
@@ -34,6 +34,29 @@ describe('repository', () => {
       ])
     );
 
+    const packageJson = JSON.parse(
+      tree.read('package.json')?.toString('utf-8') || '{}'
+    ) as {
+      devDependencies: unknown;
+    };
+
+    expect(packageJson.devDependencies).toEqual({
+      '@types/jest': '^27.0.2',
+      '@types/node': '^16.11.5',
+      '@typescript-eslint/eslint-plugin': '^5.1.0',
+      '@typescript-eslint/parser': '^5.1.0',
+      eslint: '^8.1.0',
+      'eslint-config-prettier': '^8.3.0',
+      'eslint-plugin-jest': '^25.2.2',
+      'eslint-plugin-prettier': '^4.0.0',
+      jest: '^27.3.1',
+      'jest-junit': '^13.0.0',
+      lerna: '^4.0.0',
+      prettier: '^2.4.1',
+      'ts-jest': '^27.0.7',
+      typescript: '^4.4.4',
+    });
+
     expect(tree.read('lerna.json')?.toString()).toMatch(
       /"version": "independent"/
     );
